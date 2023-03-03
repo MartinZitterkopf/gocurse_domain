c00244538eb894e0bb3867ce69693f0d0b154505
MartinZitterkopf <martin.zitterkopf@gmail.com>
2023-03-03 12:08:21 -0300

    Refactor de status

--- domain/enrollment.go
+++ domain/enrollment.go
@@ -13,12 +13,22 @@ type Enrollment struct {
 	UserID string `json:"user_id,omitempty" gorm:"type:char(36)"`
 	User   *User  `json:"user,omitempty"`
 	// CurseID va a hacer referencia a la tabla curse
-	CurseID   string     `json:"curse_id,omitempty" gorm:"type:char(36)"`
-	Curse     *Curse     `json:"curse,omitempty"`
-	Status    string     `json:"status" gorm:"type:char(2)"`
-	CreatedAt *time.Time `json:"-"`
-	UpdateAt  *time.Time `json:"-"`
+	CurseID string `json:"curse_id,omitempty" gorm:"type:char(36)"`
+	Curse   *Curse `json:"curse,omitempty"`
+
+	Status    EnrollStatus `json:"status" gorm:"type:char(2)"`
+	CreatedAt *time.Time   `json:"-"`
+	UpdateAt  *time.Time   `json:"-"`
 }
+
+type EnrollStatus string
+
+const (
+	Pending  EnrollStatus = "P"
+	Active   EnrollStatus = "A"
+	Studying EnrollStatus = "S"
+	Inactive EnrollStatus = "I"
+)
 
 func (e *Enrollment) BeforeCreate(tx *gorm.DB) (err error) {
 
